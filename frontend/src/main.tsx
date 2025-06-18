1a99df2bcc034ebd88afb9b15fb2b7cc4a10636c
aboudzeineddin <aboudzein@gmail.com>
2025-06-18 03:52:39 +0300

    feat(frontend): integrate PostHog for analytics tracking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { PostHogProvider } from 'posthog-js/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </PostHogProvider>
   </StrictMode>,
 )
